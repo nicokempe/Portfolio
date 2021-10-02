f4c69276a822a4cedb210aa65b19e3a69b562ff9
unknown <kontakt@nicokempe.de>
2021-10-02 15:01:10 +0200

    Add Error page check

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
-  <HeaderComponent />
+  <HeaderComponent v-if="hideHeader === false" />
   <div class="container">
     <router-view/>
   </div>
-  <FooterComponent />
+  <FooterComponent v-if="hideFooter === false" />
+
 </template>
 
 <script>
 import HeaderComponent from '@/components/static/HeaderComponent';
 import FooterComponent from '@/components/static/FooterComponent';
 
+
 export default {
-  components: {
-    HeaderComponent,
-    FooterComponent
+  name: 'App',
+  components: {HeaderComponent, FooterComponent},
+  data() {
+    return {
+      hideHeader: false,
+      hideFooter: false,
+      isErrorPage: false,
+    }
+  },
+  watch: {
+    '$route'(to) {
+      if (to.path === '/error') {
+        document.getElementsByTagName("body")[0].classList.add("errorPage");
+        this.hideFooter = true;
+        this.hideHeader = true;
+        this.isErrorPage = true;
+      } else {
+        document.getElementsByTagName("body")[0].classList.remove("errorPage");
+        this.hideFooter = false;
+        this.hideHeader = false;
+        this.isErrorPage = false;
+      }
+    },
   }
 }
 </script>
