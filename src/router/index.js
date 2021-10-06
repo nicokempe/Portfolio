4a024f3ab92cd8b756becf4d91f9d333579d17d8
unknown <kontakt@nicokempe.de>
2021-10-06 21:29:33 +0200

    Add the right Router links

--- src/router/index.js
+++ src/router/index.js
@@ -15,47 +15,47 @@ const routes = [
   {
     path: '/imprint',
     name: 'Imprint',
-    component: () => import('../views/ErrorView.vue')
+    component: () => import('../views/rights/ImprintView.vue')
   },
   {
     path: '/privacy-policy',
     name: 'PrivacyPolicy',
-    component: () => import('../views/ErrorView.vue')
+    component: () => import('../views/rights/PrivacyPolicy.vue')
   },
   {
     path: '/signup',
     name: 'SignUp',
-    component: () => import('../views/ErrorView.vue')
+    component: () => import('../views/user/SignUpView.vue')
   },
   {
     path: '/signin',
     name: 'SignIn',
-    component: () => import('../views/ErrorView.vue')
+    component: () => import('../views/user/SignInView.vue')
   },
   {
     path: '/signout',
     name: 'SignOut',
-    component: () => import('../views/ErrorView.vue')
+    component: () => import('../views/user/SignOutView.vue')
   },
   {
     path: '/forget-password',
     name: 'ForgetPassword',
-    component: () => import('../views/ErrorView.vue')
+    component: () => import('../views/user/ForgetPasswordView.vue')
   },
   {
     path: '/profile',
     name: 'Profile',
-    component: () => import('../views/ErrorView.vue')
+    component: () => import('../views/user/ProfileView.vue')
   },
   {
     path: '/user',
     name: 'User',
-    component: () => import('../views/ErrorView.vue')
+    component: () => import('../views/user/SignUpView.vue')
   },
   {
     path: '/settings',
     name: 'Settings',
-    component: () => import('../views/ErrorView.vue')
+    component: () => import('../views/user/SettingsView.vue')
   },
 ]
 
